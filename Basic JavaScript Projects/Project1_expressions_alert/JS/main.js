3fa9b4acf7950eea40356bbfbc8f0c27aaf96692
Catie Condit <concat2024@gmail.com>
2024-11-11 10:15:31 -0500

    Initial commit

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,22 @@
+// Displaying a simple string in an alert box
+window.alert("This is a string!"); // Prints string in an alert window
+
+// Example of printing a string with escaped quotes on the webpage
+document.write("Mike said to Joe, \"This prints the string on the actual page!\""); // Prints string with quotes on page
+
+// Creating two variables with string values
+var Sent1 = "<br>This is the beginning of the string"; // First part of the string
+var Sent2 = " and this is the end of the string.";    // Second part of the string
+
+// Concatenating Sent1 and Sent2 and printing it on the page
+document.write(Sent1 + Sent2); // Concatenates and displays the string on the page
+
+// Assigning multiple variables in a single line
+var Family = "Smith", Dad = "George", Mom = "Sue", Dog = "Raph"; // Assigning names of family members
+
+// Displaying one specific variable, Dog, from the multi-variable assignment
+document.write("<br>The dog's name is " + Dog + "."); // Prints the variable 'Dog' which is Raph
+
+// An expression example with simple arithmetic
+let x = 5 + 3;  // Expression that adds 5 and 3
+document.write("<br>The result of 5 + 3 is: " + x); // Displays the result of the expression
